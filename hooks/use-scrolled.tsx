6c054fd50ade817e81be48dcc6500a5a5bace457
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2025-08-27 23:31:00 +0530

    :icecream: scroll hook to change css

---
+++ hooks/use-scrolled.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export function useScrolled() {
+  const [scrolled, setScrolled] = React.useState(false);
+  React.useEffect(() => {
+    const onScroll = () => setScrolled(window.scrollY > 8);
+    onScroll();
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+  return scrolled;
+}
