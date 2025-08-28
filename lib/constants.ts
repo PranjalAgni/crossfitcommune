57de68212168e1a105b8789e11703efba6f32a02
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2025-08-28 13:19:28 +0530

    :refactor: constants in 1 plave

---
+++ lib/constants.ts
@@ -0,0 +1,10 @@
+export const PHONE = "918121145533";
+
+export const WHATSAPP_MESSAGE = encodeURIComponent(
+  "Hi, I’m interested in learning more about your CrossFit classes, schedule, and membership options. Could you please share the details?"
+);
+
+export const WHATSAPP_URL = `https://wa.me/${PHONE}?text=${WHATSAPP_MESSAGE}`;
+
+export const INSTAGRAM_URL =
+  "https://www.instagram.com/crossfitcommune?igsh=aGNheWplODRjdnVo";
